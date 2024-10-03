cdd0bab01419b747167cb9b63f494dde47fc96ae
ArianDannemann <arian.dannemann@gmail.com>
2024-10-03 23:04:54 +0200

    -new: Added phonebook

---
+++ pages/phonebook.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../style.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="../resources/icons/icon.ICO">
+
+        <title>ad301</title>
+    </head>
+
+    <body>
+        <div class="content inset">
+            <div class="header">
+                <p>ad301</p>
+            </div>
+
+            <p>Directory</p>
+            <ul>
+                <li><a href="../index.php">Go back <img src="../resources/icons/back.ICO" class="ico"></a></li>
+            </ul>
+
+            <hr>
+
+            <p>
+                This is a list of fun websites I found online.
+            </p>
+
+            <ul>
+                <li><a href="https://www.imood.com/">imood.com <img src="../resources/icons/extern_link.ICO" class="ico"></a></li>
+            </ul>
+        </div>
+    </body>
+</html>
